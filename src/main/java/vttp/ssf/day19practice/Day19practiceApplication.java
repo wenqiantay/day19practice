a56f0f01d408b87e0cb48f8d837ba4297227cd1a
wenqiantay <wenqianhello@gmail.com>
2024-12-03 13:55:59 +0800

    initial commit

---
+++ src/main/java/vttp/ssf/day19practice/Day19practiceApplication.java
@@ -0,0 +1,13 @@
+package vttp.ssf.day19practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day19practiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day19practiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/ssf/day19practice/Day19practiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.ssf.day19practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day19practiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
